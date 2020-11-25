7c1e0acd10e75c555d5540cdb2d37b7d9db9671f
Anatolii_Kravchenko <tol890@ukr.net>
2020-11-25 21:46:21 +0300

    check results functionality

--- script.js
+++ script.js
@@ -26,6 +26,8 @@ const choices = {
     spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
+let playerScoreNumber = 0;
+let computerScoreNumber = 0;
 let computerChoice = '';
 
 // Reset all 'selected' icons
@@ -79,16 +81,37 @@ function displayComputerChoice() {
     }
 }
 
+// Check resolts, increase scores, update resultText
+function updateScore(playerChoice) {
+    console.log(playerChoice, computerChoice);
+    if (playerChoice === computerChoice) {
+        resultText.textContent = "It's a tie";
+    } else {
+        const choice = choices[playerChoice];
+        console.log(choice.defeats.indexOf(computerChoice));
+        if (choice.defeats.indexOf(computerChoice) > -1) {
+            resultText.textContent = 'You Won!';
+            playerScoreNumber++;
+            playerScoreEl.textContent = playerScoreNumber;
+        } else {
+            resultText.textContent = 'You Lost!';
+            computerScoreNumber++;
+            computerScoreEl.textContent = computerScoreNumber;
+        }
+    }
+}
+
 // Call functions to process turn
-function checkResult() {
+function checkResult(playerChoice) {
     resetSelected();
     computerRandomChoice();
     displayComputerChoice();
+    updateScore(playerChoice);
 }
 
 // Passing player selection value and styling icons
 function select(playerChoice) {
-    checkResult();
+    checkResult(playerChoice);
     // add "selected" styling & playerCoic
     switch (playerChoice) {
         case 'rock':
